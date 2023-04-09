ecc9b81becd2339fc7730698bc58fbc493d59afc
barsa23 <iabarsa@gmail.com>
2023-04-09 02:20:57 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}'
+  ],
+  // ...
+
   theme: {
     extend: {},
   },
